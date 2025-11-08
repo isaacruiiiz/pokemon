b2c97da62d6b42e436e81399f03db25945235dab
Isaac Ruiz <isaacruiiiz04@gmail.com>
2025-11-08 21:06:06 +0100

    Add TypeScript definitions and ESLint configurations for various modules

--- pokemon/vite.config.js
+++ pokemon/vite.config.js
@@ -2,13 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pokemon/', // 👈 cambia esto al nombre EXACTO de tu repo
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/pokemon/'
+  }
 })
